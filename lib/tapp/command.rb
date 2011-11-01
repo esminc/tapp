b2170e3d29c51396d9427a3fe2144cfd9ff1c041
Keita Urashima <ursm@ursm.jp>
2011-11-01 23:44:17 +0900

    pass all arguments to grep command.

--- lib/tapp/command.rb
+++ lib/tapp/command.rb
@@ -2,9 +2,9 @@ require 'thor'
 
 module Tapp
   class Command < Thor
-    desc 'grep [GIT_GREP_OPTIONS]', 'Print lines using tapp'
-    def grep(*args)
-      system 'git', 'grep', '-w', 'tapp', *args
+    desc 'grep [<git-grep-options>]', 'Print lines using tapp'
+    def grep(*)
+      system 'git', 'grep', '--word-regexp', '-e', 'tapp', *ARGV.drop(1)
     end
   end
 end
