b84fb812c7007d37eaab3f74925eef56599fd035
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2012-03-13 17:42:44 +0900

    cosmetic

--- lib/tapp/command.rb
+++ lib/tapp/command.rb
@@ -5,7 +5,7 @@ module Tapp
     desc 'grep [<git-grep-options>]', 'Print lines using tapp'
     def grep(*)
       opts = ['--word-regexp', '-e', 'tapp', '-e', 'taputs', '-e', 'taap', *ARGV.drop(1)]
-      git_grep = ['git', 'grep', opts].flatten.join(" ")
+      git_grep = ['git', 'grep', opts].flatten.join(' ')
       puts `#{git_grep}`.gsub(/^Gemfile(\.lock)?:.+?\n/, '')
     end
   end
