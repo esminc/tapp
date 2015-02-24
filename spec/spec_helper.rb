f85b03c7cc6f92d5160920256ed3e7f55ed794bf
Keita Urashima <ursm@ursm.jp>
2015-02-24 20:55:36 +0900

    Cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,10 @@ require 'tapp/turnip'
 RSpec.configure do |config|
   config.include Tapp::Turnip::Steps
 
+  config.expect_with :rspec do |expects|
+    expects.syntax = :should
+  end
+
   config.before do
     Tapp.config.reset
   end
